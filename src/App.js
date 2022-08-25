2c9240a570763e902dd46fd5284101cf47d84911
DiegoRoney <diego-roney@outlook.com>
2022-08-25 16:15:54 -0300

    ERRO?

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <h1>ola mundo</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <br></br>
+          HELLO!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
