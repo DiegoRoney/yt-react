e8343d72597cdca0dcf7f108b75308a4a9246d95
DiegoRoney <diego-roney@outlook.com>
2022-08-25 16:34:21 -0300

    on!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
           <br></br>
           HELLO!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://diegoroney.github.io/portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DIEGO R
         </a>
       </header>
     </div>
